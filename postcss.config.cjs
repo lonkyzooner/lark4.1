f18fd61be13f1099d1e2247fece0bdcf7d3adcc2
lonkyzooner <116961432+lonkyzooner@users.noreply.github.com>
2025-03-31 19:38:32 -0500

    Remove postcss-nesting dependency and use nesting from postcss-preset-env

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,11 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('tailwindcss/nesting/index.js')(require('postcss-nesting')),
+    // Skip nesting plugin, use the one from postcss-preset-env
     require('tailwindcss'),
     require('autoprefixer'),
     require('postcss-preset-env')({
-      features: { "nesting-rules": false },
+      features: { "nesting-rules": true }, // Enable nesting here instead
       stage: 1,
       preserve: false,
       browsers: ["> 1%", "last 2 versions", "Firefox ESR", "not dead"],
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,4 @@
 import postcssImport from 'postcss-import';
-import tailwindcssNesting from 'tailwindcss/nesting/index.js';
-import postcssNesting from 'postcss-nesting';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 import postcssPresetEnv from 'postcss-preset-env';
@@ -8,11 +6,11 @@ import postcssPresetEnv from 'postcss-preset-env';
 export default {
   plugins: [
     postcssImport,
-    tailwindcssNesting(postcssNesting),
+    // Skip nesting plugin, use the one from postcss-preset-env
     tailwindcss,
     autoprefixer,
     postcssPresetEnv({
-      features: { "nesting-rules": false },
+      features: { "nesting-rules": true }, // Enable nesting here instead
       stage: 1,
       preserve: false,
       browsers: ["> 1%", "last 2 versions", "Firefox ESR", "not dead"],
